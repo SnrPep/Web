81d3a56ae24fd3e00d319da46a9f9a65b9ac0175
kand11 <125102495+kand11@users.noreply.github.com>
2024-12-19 23:45:04 +1000

    correct_vision_catalog

--- cars/views.py
+++ cars/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django_filters.views import FilterView
 from .models import Cars
 from .models import Brands
 from .filters import CarFilter
 
 def car_list(request):
-    # cars = Cars.objects.select_related('brand_country')
-    cars = Cars.objects.all()  # Получить все машины из базы
+    cars_list = Cars.objects.all()
+    paginator = Paginator(cars_list, 20)  # Показывать 50 карточек на странице
+    page_number = request.GET.get('page')
+    cars = paginator.get_page(page_number)
     return render(request, 'car_list.html', {'cars': cars})
+    # cars = Cars.objects.all()  # Получить все машины из базы
+    # return render(request, 'car_list.html', {'cars': cars})
 
 
 
