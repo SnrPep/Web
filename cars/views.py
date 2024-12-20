4e7cfbc0fffab46503a21a63581a111341383b5d
SnrPep <zubarevkostia@gmail.com>
2024-12-20 22:45:38 +1000

    filter start

--- cars/views.py
+++ cars/views.py
@@ -6,14 +6,17 @@ from .models import Brands
 from .filters import CarFilter
 
 def car_list(request):
-    cars_list = Cars.objects.all()
-    paginator = Paginator(cars_list, 20)  # Показывать 50 карточек на странице
+    car_filter = CarFilter(request.GET, queryset=Cars.objects.all())
+    paginator = Paginator(car_filter.qs, 20)
     page_number = request.GET.get('page')
     cars = paginator.get_page(page_number)
-    return render(request, 'car_list.html', {'cars': cars})
-    # cars = Cars.objects.all()  # Получить все машины из базы
-    # return render(request, 'car_list.html', {'cars': cars})
+    return render(request, 'car_list.html', {'filter': car_filter, 'cars': cars, 'request': request})
 
+    # cars_list = Cars.objects.all()
+    # paginator = Paginator(cars_list, 20)  # Показывать 20 карточек на странице
+    # page_number = request.GET.get('page')
+    # cars = paginator.get_page(page_number)
+    # return render(request, 'car_list.html', {'cars': cars})
 
 
 
