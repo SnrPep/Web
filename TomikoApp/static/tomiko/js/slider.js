3b9f11547cb03768260f7c2c3d9d769c3e9434d0
TryDovick <klest_pro_asus@mail.ru>
2024-12-24 06:11:14 +1000

    BackUp

---
+++ TomikoApp/static/tomiko/js/slider.js
@@ -0,0 +1,67 @@
+new Swiper('.image-slider');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Слайдер 
+
+let currentIndex = 0;
+const items = document.querySelectorAll('.card-item');
+const totalItems = items.length;
+
+function showSlide(index) {
+    items.forEach((item, i) => {
+        item.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function changeSlide(direction) {
+    currentIndex = (currentIndex + direction + totalItems) % totalItems;
+    showSlide(currentIndex);
+}
+
+
+showSlide(currentIndex);
+
+
+document.querySelector('.slider-btn__left').addEventListener('click', () => changeSlide(-1));
+document.querySelector('.slider-btn__right').addEventListener('click', () => changeSlide(1));
+*/
