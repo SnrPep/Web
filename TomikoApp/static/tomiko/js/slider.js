8192e1e2c0965c2ebd0851c1ff3ee602ab104d61
TryDovick <klest_pro_asus@mail.ru>
2025-01-31 00:13:14 +1000

    анимации готовы

--- TomikoApp/static/tomiko/js/slider.js
+++ TomikoApp/static/tomiko/js/slider.js
@@ -1,12 +1,38 @@
+const popup = document.getElementById('popup');
+const checkBtn = document.getElementById('check__btn');
+const closeBtn = document.querySelector('.popup__close');
+
+// Открытие попапа
+checkBtn.addEventListener('click', () => {
+    popup.classList.add('open');
+});
+
+// Закрытие попапа
+closeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.remove('open');
+});
+
+// Закрытие попапа при клике вне его
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.classList.remove('open');
+    }
+});
+
+
+// Слайдер
+const sliders = document.querySelectorAll('.slider-wrapper');
 const swiper = new Swiper('.swiper', {
     slidesPerView: 4,               // Количество видимых карточек
     spaceBetween: 20,               // Расстояние между карточками
+    
+    loop: false, 
     navigation: {
         nextEl: '.swiper-button-next',  // Элементы для переключения
         prevEl: '.swiper-button-prev',
     },
-    loop: false,                     
-
+    
     on: {
         slideChangeTransitionEnd: function () {
             
@@ -23,17 +49,46 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
+const swiperVideo = new Swiper('.swiper_clip', {
+    loop: false,
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    freeMode: true,
+    mousewheel: {
+        forceToAxis: true
+    },
+    wrapperClass: 'swiper-wrapper',
+    slideClass: 'swiper-slide-clip',
+    noSwiping: true
+});
 
-
-
-
-
-
-
-
-
-
-
+var swiperMedia = new Swiper('.media-swiper', {
+    loop: true,
+    slidesPerView: 1, // Количество слайдов, видимых одновременно
+    spaceBetween: 16, // Расстояние между слайдами
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    }
+});
 
 
 
--- TomikoApp/views.py
+++ TomikoApp/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+import redis
+import json
+import itertools
 
+redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
+
+def get_reviews():
+    reviews = []
+    keys = itertools.chain(
+        redis_client.scan_iter("2Gis_review:*"),
+        redis_client.scan_iter("VL_review:*"),
+        redis_client.scan_iter("Yandex_review:*")
+    )
+    for key in keys:
+        data = redis_client.get(key)
+        if data:
+            review = json.loads(data)
+            reviews.append(review)
+
+    return reviews[:10]  # Ограничим число отзывов
+
+def get_clips():
+    clips = []
+    keys = itertools.chain(
+        redis_client.scan_iter("VK_clip:*")
+    )
+    for key in keys:
+        data = redis_client.get(key)
+        if data:
+            clip = json.loads(data)
+            clips.append(clip)
+
+    return clips  # Ограничим число отзывов
+
+
+def home_view(request):
+    reviews = get_reviews()
+    clips = get_clips()
+    return render(request, "home.html", {"reviews": reviews, "clips": clips})
+
+# Create your views here.
 
 # Create your views here.
 def home(request):
